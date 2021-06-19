4df0b3e1da22365ee723cdf96880b124175fcb53
trapcodeio <trapcode.io@gmail.com>
2021-06-19 16:51:28 +0100

    Refactored

--- app.ts
+++ app.ts
@@ -9,7 +9,7 @@ import xpresser = require("xpresser");
 import config = require("./config");
 
 // Start Xpresser
-const $ = xpresser.init(config, {exposeDollarSign: false});
+const $ = xpresser.init(config);
 
 // Initialize Typescript for this project.
 $.initializeTypescript(__filename);
